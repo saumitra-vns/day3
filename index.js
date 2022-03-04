b354e6a2a1481e32a687b2ba07fa08daafbc6c26
saumitra-vns <97459938+saumitra-vns@users.noreply.github.com>
2022-03-04 22:59:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("lets start")
+// console.log("lets start")
+
+
+const express = require("express")
+
+const app = express()
+
+app.listen(4000, () =>{
+
+    console.log("listining on port 4000")
+})
 
